98fe40aa7c10ae3b01c17622b5deee40de142f75
Viktor Maier <viktor12-86@web.de>
2026-02-16 16:59:55 +0100

    die Frge zum weiterspielen korrigiert

--- snowman.py
+++ snowman.py
@@ -1,4 +1,10 @@
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+
+        again = input("Willst du nochmal spielen? (j/n): ").lower()
+        if again != "j":
+            print("Danke fürs spielen! Tschüss.")
+            break
